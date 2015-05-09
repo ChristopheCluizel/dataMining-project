d964659c4cca7f634527c159ae26dab4ada90971
Christophe Cluizel <christophe.cluizel@gmail.com>
2015-05-09 17:46:31 +0200

    doc(bagging): add docblock

--- src/1.ensembleClassifier(bagging)/drawBootstrap.m
+++ src/1.ensembleClassifier(bagging)/drawBootstrap.m
@@ -1,4 +1,10 @@
-%% drawBootstrap: function description
+%% drawBootstrap: generate a bag and an out-of-bag
+%% nTotal: the total size of the sample
+%% nbDraw: the number of random draws for the bag
+%% return:
+%%        bag: the bag with the indices of the data we will pick
+%%        oob: the out-of-bag with the indices of the data which will be in the out-of-bag
+
 function [bag, oob] = drawBootstrap(nTotal, nbTirage)
 
 	bag = randi(nTotal, nbTirage, 1);
