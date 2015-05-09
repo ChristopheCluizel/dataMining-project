84bdce4ebbc8c6f973345b6d06ddf2c29dc4d3c2
Thibaud <thibaud@dauce.fr>
2015-05-09 13:59:28 +0200

    fix: enlever le commentaire inutile

--- src/3.ensembleClassifier(random_forest)/implementation/rfPredict.m
+++ src/3.ensembleClassifier(random_forest)/implementation/rfPredict.m
@@ -1,6 +1,6 @@
 function probas = rfPredict(X,forest)
 
-	probas = zeros(length(X), 2); %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	probas = zeros(length(X), 2);
 
 	for i = 1:forest.nbTrees
 		probas = probas + treePredict(X,forest.trees{i});
